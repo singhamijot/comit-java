47a5ae47643073bfcd71a669db9f626fdc5b3115
unknown <singhamijot@gmail.com>
2023-05-30 19:41:44 -0600

    first commit

---
+++ src/main/java/org/comit/course/_01_variables/Variables.java
@@ -0,0 +1,24 @@
+package org.comit.course._01_variables;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Primitive variables
+		 *Class variables
+		 * */
+		 
+		
+		
+		
+		int num = 5;
+		double num2 = 5225545;
+		
+		char c1 = 'a';
+		String str1 = "hello world";
+		System.out.println("The value is: " +num2);
+		
+	}
+
+}
